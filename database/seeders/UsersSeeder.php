ee600652db3a591a0cadabad30094df4a10144a8
ValentinaA <valentina.alvarezi@autonoma.edu.co>
2022-10-03 01:30:48 -0500

    se creó el seeder de usuario con los datos

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Valentina',
+            'email' => 'valentina@gmail.com',
+            'password' => Hash::make('hola123'),
+            ]);
+    }
+}
