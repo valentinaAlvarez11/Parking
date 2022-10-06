f0d7a4d1a638860879564bb1f92e2f3b45db5fee
ValentinaA <valentina.alvarezi@autonoma.edu.co>
2022-10-05 21:36:50 -0500

    se creó la migracion tipo vehiculos

---
+++ database/migrations/2022_10_05_213624_create_tipo_vehiculos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_vehiculos', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo_vehiculo');
+            $table->float('tarifa_inicial');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_vehiculos');
+    }
+};
