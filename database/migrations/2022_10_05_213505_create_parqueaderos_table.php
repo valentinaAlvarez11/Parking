44ffc6c0e898a482ed5fc00d9f48990e9bc9bce8
ValentinaA <valentina.alvarezi@autonoma.edu.co>
2022-10-05 21:35:38 -0500

    se creó la migracion parqueadero

---
+++ database/migrations/2022_10_05_213505_create_parqueaderos_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parqueaderos', function (Blueprint $table) {
+            $table->id();
+            $table->string('lote', 2);
+            $table->boolean('disponibilidad');
+            $table->time('hora_ingreso');
+            $table->time('hora_egreso');
+            $table->date('dia');
+
+            $table->foreignId('tipoVehiculo_id');
+            $table->foreign('tipoVehiculo_id')->references('id')->on('tipoVehiculo')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parqueaderos');
+    }
+};
